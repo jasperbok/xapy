3228d4e4897dae3a49d1c8ddc32cabae351bc1c9
Jasper Bok <hello@jasperbok.nl>
2020-12-23 15:37:38 +0100

    Add methods for converting gamertag to id and vice versa

--- xapy/client.py
+++ xapy/client.py
@@ -36,3 +36,12 @@ class Client:
     def get_conversations(self):
         """Return the conversations for the authenticated user."""
         return self._get("/conversations")
+
+    def get_xuid_for_gamertag(self, gamertag: str):
+        """Return the Xbox User ID that belongs to a specific gamertag."""
+        return self._get("/xuid/{}".format(gamertag))
+
+    def get_gamertag_for_xuid(self, xuid: str):
+        """Return the gamertag that belongs to a specific Xbox User ID."""
+        res = self._get("/gamertag/{}".format(xuid), return_response=True)
+        return res.text
